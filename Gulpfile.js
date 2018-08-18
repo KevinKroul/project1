d772493cee56b5d21927c754688cee55d6bea6f8
Olga Kulikova <plastelinov@gmail.com>
2018-08-18 20:07:36 +0300

    New files
    * added gulp files

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const gulp = require("gulp");
+const browserSync = require('browser-sync').create();
+const less = require('gulp-less');
+
+
+gulp.task('less', function() {
+    return gulp.src("assets/styles/main.less")
+        .pipe(less())
+        .pipe(gulp.dest("assets/styles"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("assets/styles/components/*.less", gulp.series('less'));
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('serve'));
