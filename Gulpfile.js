555f2932b0decbdf9414fba4fbea2228f738282a
Olga Kulikova <plastelinov@gmail.com>
2018-08-25 21:09:44 +0300

    new plugins in the package.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,66 @@
 "use strict";
 
-const gulp = require("gulp");
-const browserSync = require('browser-sync').create();
-const less = require('gulp-less');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp"),
+  browserSync = require('browser-sync').create(),
+  less = require('gulp-less'),
+  autoprefixer = require('gulp-autoprefixer'),
+  path     = require("path"),
+  svgmin   = require("gulp-svgmin"),
+  rename   = require("gulp-rename"),
+  inject   = require("gulp-inject"),
+  svgstore = require("gulp-svgstore");
+
+gulp.task("svg", () => {
+  let svgs = gulp
+    .src("./assets/images/icons/*.svg")
+    .pipe(svgmin(function (file) {
+      let prefix = path.basename(file.relative, path.extname(file.relative));
+
+      return {
+        plugins: [
+          {
+            removeTitle: true
+          },
+          {
+            removeAttrs: {
+              attrs: "(fill|stroke)"
+            }
+          },
+          {
+            removeStyleElement: true
+          },
+          {
+            cleanupIDs: {
+              prefix: prefix + "-",
+              minify: true
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(rename({prefix: "icon-"}))
+    .pipe(svgstore({inlineSvg: true}));
+
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp
+    .src("index.html")
+    .pipe(inject(svgs, {transform: fileContents}))
+    .pipe(gulp.dest("./"));
+});
 
 
 gulp.task('less', function() {
     return gulp.src("assets/styles/main.less")
         .pipe(less())
-        .pipe(autoprefixer({
-
-        browsers: ['last 2 versions'],
-
-        cascade: false
-
-      }))
+        .pipe(autoprefixer())
         .pipe(gulp.dest("assets/styles"))
         .pipe(browserSync.stream());
 });
+
+
 
 gulp.task('serve', function() {
 
